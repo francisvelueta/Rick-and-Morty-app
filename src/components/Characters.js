f8e6ea2aea8b3a8bf3d278e90c1a1ba87601c715
francisvelueta <fvelueta@outlook.com>
2019-02-24 13:42:09 -0600

    add searching input

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row } from 'reactstrap';
+import { Container, Row, Input } from 'reactstrap';
 import axios from 'axios';
 
 import CharacterSummary from './CharacterSummary';
 
 
+
+
 const Characters = () => {
   const [people, setPeople ] = useState(null);
+  const [term, setTerm ] = useState('');
   const [loading, setLoading ] = useState(true);
+
+
+
 
 const getCharacters = () => {
 return axios.get('https://rickandmortyapi.com/api/character/?page=1')
@@ -19,12 +25,41 @@ return axios.get('https://rickandmortyapi.com/api/character/?page=1')
 
 }
 
+const makeSearch = string => {
+
+  if (string === "" ) {
+    getCharacters();
+  } else {
+  let regex = new RegExp(string, 'i')
+  let filtered = people.filter(r => regex.test(r.name));
+    setPeople(filtered);
+    }
+}
+
+
+
 useEffect(()=> {
   getCharacters();
 },[])
   return (
 
     <Container>
+      <form>
+        <Input
+          type="search"
+            name="term"
+            id="term"
+            placeholder="Search "
+            onChange ={e => { makeSearch(e.target.value) } }
+            onClick ={e => { makeSearch(e.target.value) } }
+            onKeyPress={e => {
+                       if (e.key !== "Enter") return
+                       makeSearch(e.target.value)
+                   }}
+
+          ></Input>
+      </form>
+
     <Row>
     { people ? people.map(p => (<CharacterSummary key ={p.id} id= {p.id} name = {p.name} image = {p.image} spiece = {p.species} status={p.status}/>)): <p>Loading...</p>}
     </Row>
