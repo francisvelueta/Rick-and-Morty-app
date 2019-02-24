37ba583a3db1d8e2af4b32f44d57b1794445c3ff
francisvelueta <fvelueta@outlook.com>
2019-02-24 00:02:20 -0600

    add grayscalein img

--- src/components/CharacterSummary.js
+++ src/components/CharacterSummary.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card,
   CardImg,
   CardText,
@@ -11,18 +11,18 @@ import axios from 'axios';
 
 
 const CharacterSummary = ({id, name, image, spiece, status}) => {
-const {status, SetStatus } = useState(null);
+let statusLife;
 
-
-
+status !== "Alive" ? statusLife = "1": statusLife = "0" 
   return (
+
     <Col  lg={4} xs ={12}>
-    <Card>
-        <CardImg top width="100%" src={image} alt="Card image character" />
-          <CardBody>
+    <Card >
+      <CardImg style={{filter: "grayscale("+statusLife+")"}} top width="100%" src={image} alt="Card image character" />
+          <CardBody >
             <CardTitle>Name: {name}</CardTitle>
             <CardSubtitle>Status: {status}</CardSubtitle>
-            <CardText>Spiece:  {spiece}</CardText>
+            <CardText>Spiece: {spiece}</CardText>
             <CardText className="form-text text-muted">ID: {id}</CardText>
             <Button color="info">More info</Button>
           </CardBody>
